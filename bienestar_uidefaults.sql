a8cff0cc4135cdce09b559ed2b2627fd4578b829
Enrique Suarez <enriquesuarezgtte@gmail.com>
2019-06-19 16:08:54 -0500

    Programa BD Bienestar

---
+++ bienestar_uidefaults.sql
@@ -0,0 +1,68 @@
+set define '^'
+set verify off
+set serveroutput on size 1000000
+set feedback off
+WHENEVER SQLERROR EXIT SQL.SQLCODE ROLLBACK
+ 
+prompt  Set Credentials...
+ 
+begin
+ 
+  -- Assumes you are running the script connected to sqlplus as the schema associated with the UI defaults or as the product schema.
+  wwv_flow_api.set_security_group_id(p_security_group_id=>2756667191444607432);
+ 
+end;
+/
+
+begin wwv_flow.g_import_in_progress := true; end;
+/
+begin 
+
+select value into wwv_flow_api.g_nls_numeric_chars from nls_session_parameters where parameter='NLS_NUMERIC_CHARACTERS';
+
+end;
+
+/
+begin execute immediate 'alter session set nls_numeric_characters=''.,''';
+
+end;
+
+/
+begin wwv_flow.g_browser_language := 'en'; end;
+/
+prompt  Check Compatibility...
+ 
+begin
+ 
+-- This date identifies the minimum version required to install this file.
+wwv_flow_api.set_version(p_version_yyyy_mm_dd=>'2019.03.31');
+ 
+end;
+/
+
+-- SET SCHEMA
+ 
+begin
+ 
+   wwv_flow_api.g_id_offset := 0;
+   wwv_flow_hint.g_schema   := 'BIENESTAR';
+   wwv_flow_hint.check_schema_privs;
+ 
+end;
+/
+
+ 
+--------------------------------------------------------------------
+prompt  SCHEMA BIENESTAR - User Interface Defaults, Table Defaults
+--
+-- Import using sqlplus as the Oracle user: APEX_190100
+-- Exported 20:45 Wednesday June 19, 2019 by: ENRIQUESUAREZGTTE@GMAIL.COM
+--
+commit;
+begin 
+execute immediate 'alter session set nls_numeric_characters='''||wwv_flow_api.g_nls_numeric_chars||'''';
+end;
+/
+set verify on
+set feedback on
+prompt  ...done
